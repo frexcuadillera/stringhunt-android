333af2aaa90818c2112364b92c817a445447b4ee
frexcuadillera <fcuadillera@gmail.com>
2019-05-06 04:18:50 +0800

    added game panel

---
+++ js/start.js
@@ -0,0 +1,11 @@
+function diffImageDown(img) {
+   img.src = "assets/ui/btn_refresh_pressed.png";
+}
+
+function diffImageUp(img) {
+	img.src = "assets/ui/btn_refresh.png";
+}
+
+function pause(){
+	window.location = 'index.html'; //temp
+}
