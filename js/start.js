853d82012315fdd183c4876f39f04f913f846f4a
frexcuadillera <fcuadillera@gmail.com>
2019-05-07 13:20:46 +0800

    0119

--- js/start.js
+++ js/start.js
@@ -5030,45 +5030,85 @@ var animationInterval = setInterval(animation, 500);
 
 function animation(){
 
-	//console.log(document.getElementById('scene-panel-player').getAttribute('src'));
-
-	// if(document.getElementById('scene-panel-player').getAttribute('src') == "assets/characters/player_0.png"){
-	// 	document.getElementById('scene-panel-player').src = "assets/characters/player_1.png";
-	// } else {
-	// 	document.getElementById('scene-panel-player').src = "assets/characters/player_0.png";
-	// }
-
-	//if(document.getElementById('scene-panel-player').getAttribute "../assets/characters/player_1.png")
-	//document.getElementById('scene-panel-player').src = "../assets/characters/player_1.png";
+	if(document.getElementById('scene-panel-player').getAttribute('src') == "assets/characters/player_0.png"){
+		document.getElementById('scene-panel-player').src = "assets/characters/player_1.png";
+	} else {
+		document.getElementById('scene-panel-player').src = "assets/characters/player_0.png";
+	}
 
 	if(currentLevel == 1) {
-	   
+
+		if(currentEnemy == 1) {
+	  
+		} else if (currentEnemy == 2) {
+		   
+		} else if (currentEnemy == 3) {
+		  
+		} else if (currentEnemy == 4) {
+		   
+		} else if (currentEnemy == 5) {
+		    
+		}
+
 	} else if (currentLevel == 2) {
-	   
+
+		if(currentEnemy == 1) {
+	  
+		} else if (currentEnemy == 2) {
+		   
+		} else if (currentEnemy == 3) {
+		  
+		} else if (currentEnemy == 4) {
+		   
+		} else if (currentEnemy == 5) {
+		    
+		}	   
+
 	} else if (currentLevel == 3) {
-	    
+
+		if(currentEnemy == 1) {
+	  
+		} else if (currentEnemy == 2) {
+		   
+		} else if (currentEnemy == 3) {
+		  
+		} else if (currentEnemy == 4) {
+		   
+		} else if (currentEnemy == 5) {
+		    
+		}	
+
 	} else if (currentLevel == 4) {
+
+		if(currentEnemy == 1) {
 	  
+		} else if (currentEnemy == 2) {
+		   
+		} else if (currentEnemy == 3) {
+		  
+		} else if (currentEnemy == 4) {
+		   
+		} else if (currentEnemy == 5) {
+		    
+		}
+
 	} else if (currentLevel == 5) {
+
+		if(currentEnemy == 1) {
 	  
+		} else if (currentEnemy == 2) {
+		   
+		} else if (currentEnemy == 3) {
+		  
+		} else if (currentEnemy == 4) {
+		   
+		} else if (currentEnemy == 5) {
+		    
+		}	
+
 	}
 		
-	if(currentEnemy == 1) {
-	  
-	} else if (currentEnemy == 2) {
-	   
-	} else if (currentEnemy == 3) {
-	  
-	} else if (currentEnemy == 4) {
-	   
-	} else if (currentEnemy == 5) {
-	    
-	}
-
 }
-
-
-
 
 //LETTER---------------------------------------------------
 
@@ -5174,7 +5214,7 @@ function getNextBoard(currentLevel){
 	    }
 	}
 
-	//generatedBoard = shuffle(generatedBoard);
+	generatedBoard = shuffle(generatedBoard);
 	currentBoard = generatedBoard;
 	return generatedBoard;
 
